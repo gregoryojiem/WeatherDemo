22471835b0cb1ece7bc5d09d9de8156caf2ee089
Gregory Ojiem <gro3228@rit.edu>
2024-11-17 14:20:54 -0500

    Add project files.

---
+++ WeatherDemo/WeatherRestAPI/WeatherAPI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MauiApp2.WeatherRestAPI
+{
+    internal class WeatherAPI
+    {
+        HttpClient _client;
+        JsonSerializerOptions _serializerOptions;
+
+
+        public WeatherAPI()
+        {
+            _client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+        public async Task<WeatherInfo> GetWeatherInfo()
+        {
+            WeatherInfo weatherInfo = new WeatherInfo();
+         
+            Uri uri = new Uri("https://api.openweathermap.org/data/2.5/weather?lat=40.7128&lon=-74.0060&units=imperial&appid=" + File.ReadAllText("api_key.txt"));
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Debug.WriteLine(content);
+                    weatherInfo = JsonSerializer.Deserialize<WeatherInfo>(content, _serializerOptions);
+                    var iconID = weatherInfo.weather[0].icon;
+                    weatherInfo.weather[0].icon = "http://openweathermap.org/img/wn/" + iconID + "@2x.png";
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+
+            return weatherInfo;
+        }
+    }
+}
